daa8d8585c301df93ee71926c591a17fa3fd9469
Rishi Raj Jain <rishi@neon.tech>
2024-12-13 19:19:39 +0530

    add og page

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,15 @@
 import type { NextConfig } from 'next'
 
-export default {} as NextConfig
+const allowedBots = '.*(bot|telegram|baidu|bing|yandex|iframely|whatsapp|facebook).*'
+
+export default {
+  async rewrites() {
+    return [
+      {
+        source: '/:match*',
+        destination: '/og_page',
+        has: [{ key: 'user-agent', type: 'header', value: allowedBots }],
+      },
+    ]
+  },
+} as NextConfig
